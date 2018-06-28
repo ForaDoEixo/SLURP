138e051c882e22ce06c9b012e686aa32daae5cf0
Niv Sardi <xaiki@evilgiggle.com>
2018-06-28 14:17:52 -0300

    fix run
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/store-listner.js
+++ src/store-listner.js
@@ -10,7 +10,7 @@ import { remote } from 'electron';
 const Twitter = remote.require('twitter')
 
 const ESClient = new elasticsearch.Client({
-  host: 'localhost:9200',
+  host: 'http://localhost:9200',
   log: 'trace'
 })
 
